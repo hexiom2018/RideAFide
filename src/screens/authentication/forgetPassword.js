dd176ec3b8b2508f0bcad7c228f135c8c14a6796
Syed UZair Ali <maazuzair3322@gmail.com>
2019-06-21 04:40:19 +0500

    forget password screen

--- src/screens/authentication/forgetPassword.js
+++ src/screens/authentication/forgetPassword.js
@@ -10,6 +10,11 @@ import IconFont from 'react-native-vector-icons/FontAwesome'
 import InputField from '../../components/inputField/InputField';
 import Button from '../../components/button/Button';
 import { Snackbar } from 'react-native-paper'
+import tick from '../../../assets/email/tick-checked.png'
+import Untick from '../../../assets/email/untick.png'
+import mail from '../../../assets/settings.png'
+import scan from '../../../assets/Scan.png'
+import logo from '../../../assets/email/logo.png'
 
 
 class ForgetPassword extends React.Component {
@@ -49,7 +54,7 @@ class ForgetPassword extends React.Component {
             var xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function () {
                 console.log(this.response, 'eeskdjbsak')
-                if(this.status === 200 && !count) {
+                if (this.status === 200 && !count) {
                     count = 1
                     that.setState({
                         alert: true,
@@ -58,14 +63,14 @@ class ForgetPassword extends React.Component {
                         text: 'Please check your email'
                     })
                 }
-                else if(this.status === 401 && !count) {
+                else if (this.status === 401 && !count) {
                     that.setState({
                         alert: true,
                         loading: false,
                         text: 'A user with that email does not exist'
                     })
                 }
-                else if(this.state && !count) {
+                else if (this.state && !count) {
                     count = 1
                     that.setState({
                         alert: true,
@@ -80,13 +85,13 @@ class ForgetPassword extends React.Component {
             xhttp.send(`user_login=${email}`);
 
 
-            
+
             // this.setState({
             //     alert: true,
             //     text: err.message,
             //     loading: false,
             // })
-            
+
         } else {
             this.setState({
                 alert: true,
@@ -103,27 +108,45 @@ class ForgetPassword extends React.Component {
     render() {
         const { email, text, checkEmail, loading } = this.state
         return (
-            <KeyboardAvoidingView style={{ flex: 1, position: 'relative', backgroundColor: '#3498db' }} behavior={'padding'} enabled>
-                {/* <View> */}
-                {/* <View style={styles.header}>
-                    <TouchableOpacity onPress={() => this.goBack()} activeOpacity={0.7}>
-                        <View style={styles.Icon}>
+
+
+
+            <KeyboardAvoidingView style={{ flex: 1, position: 'relative', }} behavior={'padding'} enabled>
+                <View style={{ flexDirection: 'row', paddingVertical: '6%' }}>
+                    <View style={{ width: '60%', paddingLeft: 15, height: 50, justifyContent: 'center' }}>
+                        <Image
+                            // style={{ width: 100, height: 100 }}
+                            source={logo}
+                        />
+                    </View>
+                    <View style={{ paddingHorizontal: '2%', height: 50, borderWidth: 1, borderColor: "#5dc5c0", flexDirection: 'column', alignItems: 'center', }}>
+                        <TouchableOpacity activeOpacity={0.7}>
                             <Image
-                                style={{ width: '100%', height: '100%' }}
-                                source={BackIcon}
+                                // style={{ width: '100%', height: '100%' }}
+                                source={mail}
                             />
-                        </View>
-                    </TouchableOpacity>
-                    <View style={{ alignSelf: 'center', paddingRight: 20, flexGrow: 1, alignItems: 'center' }}>
-                        <Text style={{ fontSize: 18, fontWeight: '400' }}>
-                            {'Glemt dit password?'}
+                        </TouchableOpacity>
+                        <Text style={{ fontSize: 12, color: '#5dc5c0' }}>
+                            {'Settings'}
+                        </Text>
+
+                    </View>
+                    <View style={{ paddingHorizontal: '2%', height: 50, marginRight: '5%', flexDirection: 'column', alignItems: 'center' }}>
+                        <TouchableOpacity activeOpacity={0.7}>
+                            <Image
+                                // style={{ width: '100%', height: '100%' }}
+                                source={scan}
+                            />
+                        </TouchableOpacity>
+                        <Text style={{ fontSize: 12 }} >
+                            {'Scan'}
                         </Text>
                     </View>
-                </View> */}
+                </View>
                 <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'center', paddingVertical: 40 }}>
                     <View>
                         <View style={styles.form}>
-                            <InputField
+                            {/* <InputField
                                 label={'E-mail'}
                                 name={'email'}
                                 type={'email-address'}
@@ -132,7 +155,30 @@ class ForgetPassword extends React.Component {
                                 secure={false}
                                 fontAwesome={false}
                                 change={(value) => this.onChange(value, 'email')}
-                            />
+                            /> */}
+
+                            <View style={{ width: '80%' }}>
+                                <TextInput
+                                    keyboardType={'email-address'}
+                                    placeholder={'Enter email here'}
+                                    placeholderTextColor={'#686868'}
+                                    change={(value) => this.onChange(value, 'email')}
+                                    // value={username}
+                                    textContentType={'emailAddress'}
+                                    style={{
+                                        borderWidth: 1,
+                                        color: '#6a6a6a',
+                                        borderColor: '#77d8c5',
+                                        textAlign: 'center',
+                                        paddingHorizontal: 10,
+                                        paddingVertical: 10,
+                                        borderRadius: 7,
+                                        fontStyle: 'italic'
+                                    }}
+                                />
+                            </View>
+
+
                             {
                                 checkEmail &&
                                 <View style={{ width: '90%', alignItems: 'center' }}>
@@ -142,15 +188,18 @@ class ForgetPassword extends React.Component {
                                 </View>
                             }
                         </View>
-                        <View style={styles.button}>
-                            <Button
-                                color={true}
-                                border={true}
-                                name={'send'}
-                                background={true}
-                                buttonAction={() => this.forgetPassword()}
-                                textColor={'white'}
-                            />
+                        <View style={{ alignItems: 'center', marginTop: '20%' }}>
+                            <TouchableOpacity onPress={() => this.forgetPassword()} activeOpacity={0.7} style={{ width: '70%', backgroundColor: '#77d8c5', borderColor: '#7ad6c5', borderWidth: 1, paddingVertical: 2, borderRadius: 10 }}>
+                                {
+                                    !loading &&
+                                    <View>
+                                        <Text style={{ textAlign: 'center', fontSize: 18, color: 'white' }}>
+                                            {'Send'}
+                                        </Text>
+                                    </View>
+                                }
+                                {loading && <ActivityIndicator size="small" color="#00ff00" />}
+                            </TouchableOpacity>
                         </View>
                     </View>
                 </ScrollView>
@@ -173,7 +222,7 @@ class ForgetPassword extends React.Component {
                         <ActivityIndicator size="large" color="blue" />
                     }
                 </View>
-            </KeyboardAvoidingView>
+            </KeyboardAvoidingView >
         )
     }
 }
